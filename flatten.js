2d6753fcdf87230da878f407876bec53743e2f2d
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-19 19:09:28 -0400

    Implement code for flatten function

--- flatten.js
+++ flatten.js
@@ -1,11 +1,20 @@
 var flatten = function(nestedArray, result) {
 
     // initialize result as empty array when first called
+    result = result || [];
 
     // loop through array
- 
-      // if current array isn't a nested array add it the result array
-
+    for(var i = 0; i < nestedArray.length; i++) {
+      var currentItem = nestedArray[i];
+      // if current array isn't a nested array push it into the result array
+      if (!Array.isArray(currentItem)) {
+        result.push(currentItem);
       // otherwise, call the function recursively on the nested array
+      } else if(Array.isArray(currentItem)) {
+        flatten(currentItem, result);
+      };
+    }
 
-    // return result array
+    return result;
+
+  };
