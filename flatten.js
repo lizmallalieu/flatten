61af4dcd1defab4495e035d6fc483de828551806
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-19 19:08:53 -0400

    Write pseudocode

---
+++ flatten.js
@@ -0,0 +1,11 @@
+var flatten = function(nestedArray, result) {
+
+    // initialize result as empty array when first called
+
+    // loop through array
+ 
+      // if current array isn't a nested array add it the result array
+
+      // otherwise, call the function recursively on the nested array
+
+    // return result array
