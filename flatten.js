32f085a2fd6525d6c5a8258c8ea26613a28ee2a0
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-19 19:15:20 -0400

    Change indentation

--- flatten.js
+++ flatten.js
@@ -1,20 +1,20 @@
 var flatten = function(nestedArray, result) {
 
-    // initialize result as empty array when first called
-    result = result || [];
+  // initialize result as empty array when first called
+  result = result || [];
 
-    // loop through array
-    for(var i = 0; i < nestedArray.length; i++) {
-      var currentItem = nestedArray[i];
-      // if current array isn't a nested array push it into the result array
-      if (!Array.isArray(currentItem)) {
-        result.push(currentItem);
-      // otherwise, call the function recursively on the nested array
-      } else if(Array.isArray(currentItem)) {
-        flatten(currentItem, result);
-      };
-    }
+  // loop through array
+  for(var i = 0; i < nestedArray.length; i++) {
+    var currentItem = nestedArray[i];
+    // if current array isn't a nested array push it into the result array
+    if (!Array.isArray(currentItem)) {
+      result.push(currentItem);
+    // otherwise, call the function recursively on the nested array
+    } else if(Array.isArray(currentItem)) {
+      flatten(currentItem, result);
+    };
+  }
 
-    return result;
+  return result;
 
 };
