f3ded292a4e864902a3ef5ab3f8571c10cfc20d3
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-19 23:53:48 -0400

    Add test that includes null values

--- spec/spec.js
+++ spec/spec.js
@@ -8,5 +8,12 @@
 
       expect(flatten(array)).to.eql([1,2,3,4]);
     });
+
+    it('can flatten nested arrays including null values', function() {
+      var array = [0, 2, [[2, 3], 8, 100, null, [[null]]], -2];
+
+      expect(flatten(array)).to.eql([0, 2, 2, 3, 8, 100, -2]);
+    });
+
   });
 }());
