4b146ec02c49b3629d882513b05b94aa73d9f33d
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-20 09:59:03 -0400

    Add test for nested empty arrays

--- spec/spec.js
+++ spec/spec.js
@@ -3,16 +3,22 @@
 
   describe('flatten', function() {
 
-    it('can flatten nested arrays', function() {
+    it('should flatten nested arrays', function() {
       var array = [1, [2], [3, [[[4]]]]];
 
       expect(flatten(array)).to.eql([1,2,3,4]);
     });
 
-    it('can flatten nested arrays including null values', function() {
+    it('should flatten nested arrays including null values', function() {
       var array = [0, 2, [[2, 3], 8, 100, null, [[null]]], -2];
 
       expect(flatten(array)).to.eql([0, 2, 2, 3, 8, 100, -2]);
+    });
+
+    it('should flatten nested empty arrays', function() {
+      var array = [[[]], [[], []], [[]]];
+
+      expect(flatten(array)).to.eql([]);
     });
 
   });
